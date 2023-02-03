3684ebdda6b964094bc29c11bba114a2a6d92641
imagineyourselfs <i00nsu@disroot.org>
2023-02-03 18:23:01 +0000

    Usa este script em caso de dúvidas e lê o ficheiro SUGIRO.md

--- pushit.sh
+++ pushit.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Não precisas de mudar as permissões do ficheiro para executável
+# Basta usares: bash pushit.sh 
+# E o commit é feito sem pedir dados de nada! 
+
 git push git@github.com:lanparti/organiza
