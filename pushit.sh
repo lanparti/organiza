56e6483cdc1013e3ba23f3a6cfe28b447cf4135d
OM <i00nsu@riseup.net>
2023-02-22 02:45:12 +0000

    melhoria do script

--- pushit.sh
+++ pushit.sh
@@ -2,5 +2,8 @@
 # Não precisas de mudar as permissões do ficheiro para executável
 # Basta usares: bash pushit.sh 
 # E o commit é feito sem pedir dados de nada! 
-
+pkill -x ssh-agent # porque uso outros dominios
+sleep 1
+eval $(ssh-agent)
+sleep 1 
 git push git@github.com:lanparti/organiza
